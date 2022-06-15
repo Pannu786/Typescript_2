c62ff8f9172957c1d72dc199d0e336c5dffce8f6
Pannu786 <thepannu786@gmail.com>
2022-06-15 21:59:47 +0530

    object started

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,11 @@ console.log(mySize);
 
 // Functions
 
-function calculateTax(income: number) {
-  if (income < 50_000) return income * 0.2;
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 2022) return income * 1.2;
   return income * 1.3;
 }
+
+calculateTax(1000);
+
+// object
